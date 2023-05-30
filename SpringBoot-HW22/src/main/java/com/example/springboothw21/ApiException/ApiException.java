f4e75b36041e57f8c0a62450a8574758e389be4f
Ghadeer <102277108+iigh1@users.noreply.github.com>
2023-05-30 16:21:26 +0300

    Delete SpringBoot-HW22 directory

--- SpringBoot-HW22/src/main/java/com/example/springboothw21/ApiException/ApiException.java
+++
@@ -1,8 +0,0 @@
-package com.example.springboothw21.ApiException;
-
-public class ApiException extends RuntimeException{
-
-    public ApiException(String message){
-        super(message);
-    }
-}
